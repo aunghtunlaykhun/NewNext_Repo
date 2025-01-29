b5fb7fd98cbe5bce2452dbec74191601edbedeb8
KhunAungHtunLay <jotaro097505@gmail.com>
2025-01-30 02:27:16 +0630

    Assets & Metada

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <>
-      <h1 className="bg-black text-white shadow-sm">hello sdfsdfsdfsdf  fdfsfs fefewfw sdffdsfsdf fwefw fwfwf fsfsdf fwefwef </h1>
+      <h1 className=" text-primary-500 shadow-sm">HELLO MY NAME IS </h1>
+      <h1 className="font-inter text-primary-500 shadow-sm">HELLO MY NAME IS </h1>
+      <h1 className="font-space-grotesk text-primary-500 shadow-sm">HELLO MY NAME IS </h1>
+
     </>
   );
 }
