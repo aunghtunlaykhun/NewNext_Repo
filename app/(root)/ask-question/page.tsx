0627e8316f75384ba8bda6141710e21b1a363a3f
KhunAungHtunLay <jotaro097505@gmail.com>
2025-02-05 22:20:34 +0630

    implement question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import QuestionForm from '@/components/forms/QuestionForm'
+
 const AskaQuestion = () => {
   return (
-    <div>AskaQuestion</div>
+    <>
+      <h1 className='h1-bold text-dark100_light900 '>QuestionForm</h1>
+        <div className='mt-9 '>
+          <QuestionForm/>
+      </div>
+    </>
   )
 }
 
